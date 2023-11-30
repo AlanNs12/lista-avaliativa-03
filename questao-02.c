b2603efecb115db45748f4fe33c1a5dfc03281d6
Alan Nascimento <alannascimento.contato@gmail.com>
2023-11-30 16:34:36 -0300

    questao2-concluida

--- questao-02.c
+++ questao-02.c
@@ -1,14 +1,22 @@
 #include <stdio.h>
-#include <stdlib.h>
+#include <math.h>
 
-int main(){
-    void* investMes;
-    investMes = (void*) malloc(5*sizeof(void));
+int main() {
+    int meses;
+    double aporte, taxaJuros, M;
+    
+    scanf("%d", &meses);
+    scanf("%lf", &aporte);
+    scanf("%lf", &taxaJuros);
 
-    scanf("%d",);
+    double fator = 1.0 + taxaJuros;
 
+    for (int tempo = 1; tempo <= meses; tempo++) {
+        double termo_potencia = pow(fator, tempo);
 
-
+        M = aporte * fator * ((termo_potencia - 1) / taxaJuros);
+        printf("Montante ao fim do mes %d: R$ %.2lf\n", tempo, M);
+    }
 
     return 0;
 }
